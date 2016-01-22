f59c1f5e5e5f28e9850afb132508f0946e7f500c
mlinsenbard <mlinsenb@calpoly.edu>
2016-01-22 00:39:29 -0800

    adding death scene, will make new branch to work on enemy damage.

---
+++ scenes/death.js
@@ -0,0 +1,32 @@
+/*
+ * The ur dead state of the game.
+ */
+
+/*
+ * So this is where Enchant gets weird. Refer to utils/singleton.js to see more...
+ */
+Scenes.Death = Singletonify(function(game) {
+   var Death = new Scene();
+   Death.backgroundColor = "black";
+   
+   var labels = [
+      Utils.createLabel('YOU HAVE DIED', 50, 50, { font: '28px serif' }),
+      Utils.createLabel('Press space to return to menu', 50, GAME_HEIGHT - 50, { font: '14px sans-serif' }),
+   ];
+
+   labels.forEach(function(label) {
+      Death.addChild(label);
+   });
+   
+   Death.addEventListener(Event.INPUT_START, function() {
+      if (game.input.select) {
+         var newSound = game.assets['assets/sounds/select2.wav'].clone();
+         newSound.play();
+
+         // Return to title screen
+         game.popScene();
+      }
+   });
+
+   return Death;
+});
