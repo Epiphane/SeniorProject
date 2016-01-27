194356cacb8823c006f0986b77bdc1b9dc6f57d7
Thomas Steinke <exyphnos@gmail.com>
2016-01-27 14:28:48 -0800

    Floor modification

--- classes/room_generator_combat.js
+++ classes/room_generator_combat.js
@@ -5,6 +5,18 @@
    var defaults = {};
 
    window.CombatRoomGenerator = Class.create(RoomGenerator, {
+      createFloor: function(params) {
+         var background = RoomGenerator.prototype.createFloor.apply(this, arguments);
+
+         for (var y = -params.height_2 + 1; y < params.height_2 - 1; y ++) {
+            for (var x = -params.width_2 + 1; x < params.width_2 - 1; x ++) {
+               // if (chance.bool({ likelihood: 14 })) this.setTile(background, x, y, C.BG_TILES.floor_blocked);
+            }  
+         }
+
+         return background;
+      },
+
       populateRoom: function(room) {
          // Add enemies and items to room
          this.addCharacter(room, new Classes.Slime(), 2, 3);
