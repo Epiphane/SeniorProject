8e65417bdd15dfb25ff8a0dc1ea16a8bb80e7661
elliotfiske <elliotfiske@gmail.com>
2016-02-09 18:39:52 -0800

    Debugarino

--- classes/room_generator_puzzle.js
+++ classes/room_generator_puzzle.js
@@ -7,6 +7,7 @@
    window.PuzzleRoomGenerator = Class.create(RoomGenerator, {
       populateRoom: function(room) {
          // Add boulders and switches to the room
+         this.addCharacter(room, new Classes.Slime(), 2, 3);
          room.addItemAt(new Classes.Pushable(), 4, 4);
       }
    });
