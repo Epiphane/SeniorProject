d53a44e5494b9531f46b99cc1b72b58932b8b8f2
elliotfiske <elliotfiske@gmail.com>
2016-02-25 18:13:05 -0800

    TABS? MORE LIKE BABS

--- classes/pushable.js
+++ classes/pushable.js
@@ -1,7 +1,7 @@
 ClassManager.create('Pushable', function(game) {
    return Class.create(Classes.Character, {
-		itemName: 'shield_4',
-		className: 'Pushable',
+      itemName: 'shield_4',
+      className: 'Pushable',
 
 
       initialize: function(x, y) {
@@ -9,27 +9,27 @@ ClassManager.create('Pushable', function(game) {
          this.image = game.assets["assets/images/boulder.png"];
       },
 
-		act: function(something) {
-			// Fun stuff
-			console.log("Act received... " + something);
+      act: function(something) {
+         // Fun stuff
+         console.log("Act received... " + something);
 
-			
-		},
+         
+      },
 
-		// Check if it's valid to push a boulder in a direction
-		tryPushInDirection: function(dx, dy, room) {
-			var canPush = room.isPlayerWalkable(this.position.x + dx, this.position.y + dy, dx, dy);
+      // Check if it's valid to push a boulder in a direction
+      tryPushInDirection: function(dx, dy, room) {
+         var canPush = room.isPlayerWalkable(this.position.x + dx, this.position.y + dy, dx, dy);
 
-   		if (canPush) {
-				this.position.x += dx;
-				this.position.y += dy;
-   		}
-			
-			return canPush;
-		},
+         if (canPush) {
+            this.position.x += dx;
+            this.position.y += dy;
+         }
+         
+         return canPush;
+      },
 
-		doAI: function() {
-			// Boulders don't have much in the way of intelligence
-		}
- 	});
+      doAI: function() {
+         // Boulders don't have much in the way of intelligence
+      }
+    });
 });
--- classes/unlockable_door.js
+++ classes/unlockable_door.js
@@ -3,43 +3,43 @@
 //  only opens when all the swithces are pressed
 ClassManager.create('UnlockableDoor', function(game) {
    return Class.create(Classes.Item, {
-		itemName: 'shield_3',
-		className: 'UnlockableDoor',
+      itemName: 'shield_3',
+      className: 'UnlockableDoor',
 
-		connectedSwitches: [],
-		impassible: true,
+      connectedSwitches: [],
+      impassible: true,
 
-		act: function(who_cares) {
-			// Maybe later, it'll highlight the switches you need to press
-			console.log("Act received... " + who_cares);
-		},
+      act: function(who_cares) {
+         // Maybe later, it'll highlight the switches you need to press
+         console.log("Act received... " + who_cares);
+      },
 
-		switchChanged: function(changedSwitch, isPressed) {
-			var shouldOpen = true;
-			connectedSwitches.forEach(function(currSwitch) {
-				if (currSwitch == changedSwitch) {
-					currSwitch.pressed = isPressed;
-				}
+      switchChanged: function(changedSwitch, isPressed) {
+         var shouldOpen = true;
+         connectedSwitches.forEach(function(currSwitch) {
+            if (currSwitch == changedSwitch) {
+               currSwitch.pressed = isPressed;
+            }
 
-				if (!currSwitch.pressed) {
-					shouldOpen = false;
-				}
-			});
+            if (!currSwitch.pressed) {
+               shouldOpen = false;
+            }
+         });
 
-			if (shouldOpen) {
-				impassible = false;
-				console.log("Door is open...");
-				this.image = game.assets["assets/images/boulder.png"];
-			}
-			else {
-				impassible = true;
-				console.log("Door is closed!");
-				this.image = game.assets["assets/images/door.png"];
-			}
-		},
-		
-		doAI: function() {
-			// Doors don't have much in the way of intelligence
-		}
- 	});
+         if (shouldOpen) {
+            impassible = false;
+            console.log("Door is open...");
+            this.image = game.assets["assets/images/boulder.png"];
+         }
+         else {
+            impassible = true;
+            console.log("Door is closed!");
+            this.image = game.assets["assets/images/door.png"];
+         }
+      },
+      
+      doAI: function() {
+         // Doors don't have much in the way of intelligence
+      }
+    });
 });
