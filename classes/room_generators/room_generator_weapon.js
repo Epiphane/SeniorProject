00cde92e470014b0ae033e14346fe1ef0c8b955a
Thomas Steinke <exyphnos@gmail.com>
2016-06-02 08:53:18 -0700

    Weapon puzzle?

--- classes/room_generators/room_generator_weapon.js
+++ classes/room_generators/room_generator_weapon.js
@@ -4,21 +4,21 @@
 (function(window) {
    var defaults = {};
 
-   window.WeaponRoomGenerator = Class.create(RoomGenerator, {
+   window.WeaponRoomGenerator = Class.create(PuzzleRoomGenerator, {
       createFloor: function(params) {
-         var background = RoomGenerator.prototype.createFloor.apply(this, arguments);
+         var background = PuzzleRoomGenerator.prototype.createFloor.apply(this, arguments);
 
-         this.setTile(background,  1,  1, C.BG_TILES.floor_blocked);
-         this.setTile(background,  1, -1, C.BG_TILES.floor_blocked);
-         this.setTile(background, -1, -1, C.BG_TILES.floor_blocked);
-         this.setTile(background, -1,  1, C.BG_TILES.floor_blocked);
+         this.setTile(background,  0,  0, C.BG_TILES.prize_weapon);
 
          return background;
       },
 
       populateRoom: function(room) {
+         PuzzleRoomGenerator.prototype.populateRoom.apply(this, arguments);
+
          var weapon = chance.pick(C.Weapons);
-         this.addItem(room, new Classes[weapon](), 0, 0);
+         // this.addItem(room, new Classes[weapon](), 0, 0);
+         room.prize = new Classes[weapon]();
       }
    });
 })(window);
